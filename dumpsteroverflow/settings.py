054ccb4bbfcc010c1d1144396b1448df71d8530b
Alejandro Castillo <castillochaves@gmail.com>
2014-06-21 21:33:23 +0200

    Fucking import

--- dumpsteroverflow/settings.py
+++ dumpsteroverflow/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from urllib import urlparse
+from urlparse import urlparse
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 HOME_DIR = os.path.join(BASE_DIR, 'dumpsteroverflow')
 
