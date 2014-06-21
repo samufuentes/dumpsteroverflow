222fafbe1b0d9342704887d123122ba23aea9c6a
Samuel Fuentes <info@samuelfuentes.com>
2014-06-21 23:59:47 +0200

    Create full dumpster

--- dumpsteroverflow/do_core/views.py
+++ dumpsteroverflow/do_core/views.py
@@ -14,15 +14,15 @@ def home(request):
             print "Form is valid!!"
             # Reset form since we come back to initial view instead of redirect.
             # TODO. Update context: points and point variation
-            # address = Address.objects.get_or_create(street_address=form.cleaned_data['street_address'],
-            #     zip_code=form.cleaned_data['zip_code'], city=form.cleaned_data['city'])
-            # if form.cleaned_data['is_brown']:
-            #     dumpster = Dumpster.objects.get_or_create(dumpster_type=Dumpster.DUMPSTER_TYPE[0], location=address)
-            #     if not dumpster.is_full:
-            #         dumpster.is_full = True
-            #         dumpster.save()
-            #         # TODO: Update points
-            #         # TODO: Send alert to garbage collectors
+            address, created = Address.objects.get_or_create(street_address=form.cleaned_data['street_address'],
+                zip_code=form.cleaned_data['zip_code'], city=form.cleaned_data['city'])
+            if form.cleaned_data['is_brown']:
+                dumpster, created = Dumpster.objects.get_or_create(dumpster_type=Dumpster.DUMPSTER_TYPES[0], location=address)
+                if not dumpster.is_full:
+                    dumpster.is_full = True
+                    dumpster.save()
+                    # TODO: Update points
+                    # TODO: Send alert to garbage collectors
             form = OverflowForm()
     print form
     return render(request, template_name, context)
