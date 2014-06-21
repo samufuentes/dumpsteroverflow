45fdcbda20eeebf38f378ad89808de3ccec84f4a
Samuel Fuentes <info@samuelfuentes.com>
2014-06-21 21:02:34 +0200

    Add form (not tested)

--- dumpsteroverflow/do_core/views.py
+++ dumpsteroverflow/do_core/views.py
@@ -1,9 +1,24 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from forms import OverflowForm
+from models import Address, Dumpster
 
-class HomeView(TemplateView):
+class HomeView(FormView):
     template_name = 'home.html'
+    form_class = OverflowForm
+    success_url = '/'
 
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        # Change context here
-        return context
+    def form_valid(self, form):
+        address = Address.objects.get_or_create(street_address=form.cleaned_data['street_address'],
+            zip_code=form.cleaned_data['zip_code'], city=form.cleaned_data['city'])
+        if form.cleaned_data['is_brown']:
+            dumpster = Dumpster.objects.get_or_create(dumpster_type=Dumpster.DUMPSTER_TYPE[0], location=address)
+            if not dumpster.is_full:
+                dumpster.is_full = True
+                # TODO: Update points
+                # TODO: Send alert to garbage collectors
+        return super(HomeView, self).form_valid(form)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(HomeView, self).get_context_data(**kwargs)
+    #     # Change context here
+    #     return context
