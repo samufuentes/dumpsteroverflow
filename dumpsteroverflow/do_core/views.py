74f94a12fba64e894754fb60dc839ce24a53ec0f
Patricia Garcia <pat@patriciagarcia.me>
2014-06-21 21:00:32 +0200

    Add dumpster selection form

--- dumpsteroverflow/do_core/views.py
+++ dumpsteroverflow/do_core/views.py
@@ -1,9 +1,12 @@
 from django.views.generic.base import TemplateView
 
+from forms import OverflowForm
+
 class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
+        context["form"] = OverflowForm()
         # Change context here
         return context
