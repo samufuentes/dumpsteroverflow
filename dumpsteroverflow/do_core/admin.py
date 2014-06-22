565a55a6a41cc8db12a6da440245083db321a4a1
Samuel Fuentes <info@samuelfuentes.com>
2014-06-22 09:54:32 +0200

    More admin changes

--- dumpsteroverflow/do_core/admin.py
+++ dumpsteroverflow/do_core/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from models import Discoverer, Dumpster, Address, Overflow
 
+class DumpsterAdmin(admin.ModelAdmin):
+    list_display = ('dumpster_type', 'is_full', 'modified_at')
+    list_filter = ['modified_at']
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('street_address', 'zip_code', 'city', 'modified_at')
+
 admin.site.register(Discoverer)
-admin.site.register(Dumpster)
-admin.site.register(Address)
+admin.site.register(Dumpster, DumpsterAdmin)
+admin.site.register(Address, AddressAdmin)
 admin.site.register(Overflow)
