f312ed45f4586a1aa4adb036c69292ca826ce577
Samuel Fuentes <info@samuelfuentes.com>
2014-06-22 09:00:11 +0200

    Add all containers

--- dumpsteroverflow/do_core/views.py
+++ dumpsteroverflow/do_core/views.py
@@ -13,7 +13,6 @@ def home(request):
     if request.method == 'POST':
         form = OverflowForm(request.POST)
         if form.is_valid():
-            # TODO. Update context: points and point variation
             points = 0
             address, created = Address.objects.get_or_create(street_address=form.cleaned_data['street_address'],
                 zip_code=form.cleaned_data['zip_code'], city=form.cleaned_data['city'])
@@ -22,11 +21,26 @@ def home(request):
                 if not dumpster.is_full:
                     dumpster.is_full = True
                     dumpster.save()
+                    points += 3
+            if form.cleaned_data['is_yellow']:
+                dumpster, created = Dumpster.objects.get_or_create(dumpster_type=Dumpster.DUMPSTER_TYPES[1], location=address)
+                if not dumpster.is_full:
+                    dumpster.is_full = True
+                    dumpster.save()
+                    points += 3
+            if form.cleaned_data['is_blue']:
+                dumpster, created = Dumpster.objects.get_or_create(dumpster_type=Dumpster.DUMPSTER_TYPES[2], location=address)
+                if not dumpster.is_full:
+                    dumpster.is_full = True
+                    dumpster.save()
+                    points += 3
+            if form.cleaned_data['is_gray']:
+                dumpster, created = Dumpster.objects.get_or_create(dumpster_type=Dumpster.DUMPSTER_TYPES[3], location=address)
+                if not dumpster.is_full:
+                    dumpster.is_full = True
+                    dumpster.save()
                     # TODO: Update points
-                    points = 3
-                    # TODO: Send alert to garbage collectors
-                else:
-                    points = 0
+                    points += 3                   
 
             return HttpResponseRedirect(reverse('overflow', args=(points,)))
     return render(request, template_name, context)
